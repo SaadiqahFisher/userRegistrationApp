9e8fb5fdc8c7f18fb545f9070e0e51fe59369d34
SaadiqahFisher <115870866+SaadiqahFisher@users.noreply.github.com>
2023-03-10 05:04:28 +0200

    urc
    2.3

--- user-registration-challenge/src/js/user-register.js
+++ user-registration-challenge/src/js/user-register.js
@@ -7,10 +7,14 @@
     // Get reference to all elements in register tab
     const registerTab = document.getElementsByClassName("register");
     console.log(registerTab);
+    let registerTab1 = registerTab[0];
+    let registerTab2 = registerTab[1];
 
     // Get reference to all elements in userList tab
     const userTabs = document.getElementsByClassName("users");
     console.log(userTabs);
+    let userTab1 = registerTab[0];
+    let userTab = registerTab[1];
 
 
 /* --- Nav references --- */
