75b075877d67e64470a330111ac159e840985a5e
SaadiqahFisher <115870866+SaadiqahFisher@users.noreply.github.com>
2023-03-10 22:16:01 +0200

    urc
    3

--- user-registration-challenge/src/js/user-register.js
+++ user-registration-challenge/src/js/user-register.js
@@ -16,6 +16,20 @@
     let userTab1 = registerTab[0];
     let userTab = registerTab[1];
 
+    // Switch to user tab
+    document.getElementById("register-tab-button").addEventListener("click",function(){
+        userTabs[0].style.display = "block";
+        registerTab[0].style.display = "none";
+        userTabs[1].style.display = "block";
+        registerTab[1].style.display = "none";
+    })
+
+    // Swith User list tab
+    usersTabButton.addEventListener("click",function(){
+        userTabs.style.display = "block";
+        registerTab.style.display = "none";
+    })
+
 
 /* --- Nav references --- */
 
